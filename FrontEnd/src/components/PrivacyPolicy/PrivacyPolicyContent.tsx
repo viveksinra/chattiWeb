dc54b1e8fb15eb8690fe900510c24062b48726e4
viveksinra <vivekkrvks@gmail.com>
2024-04-20 17:15:28 +0530

    privacy policy

--- FrontEnd/src/components/PrivacyPolicy/PrivacyPolicyContent.tsx
+++ FrontEnd/src/components/PrivacyPolicy/PrivacyPolicyContent.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 
 const PrivacyPolicyContent: React.FC = () => {
@@ -7,110 +5,60 @@ const PrivacyPolicyContent: React.FC = () => {
     <>
       <div className="main-content ptb-80">
         <div className="container">
-          <h3>What is Lorem Ipsum?</h3>
+          <h3>Chatti Se Mandi Tak - Privacy Policy</h3>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Effective date: 10/04/2024<br/>
+            This Privacy Policy describes how Chatti Se Mandi Tak ("we", "us", or "our") collects, uses, and discloses your information when you use our mobile application (the "App").
+          </p>
+          
+          <h4>Information We Collect</h4>
+          <p>We may collect the following information when you use the App:</p>
+          <ul>
+            <li><strong>Required Information:</strong></li>
+            <li>Mobile Number: We collect your mobile number to create your account and enable certain features within the App.</li>
+            <li><strong>Optional Information:</strong></li>
+            <li>Name: You can choose to provide your name for better profile identification.</li>
+            <li>User Image: You can choose to upload a user image for your profile.</li>
+            <li>Address: You can optionally provide your address for specific features within the App, such as delivery services.</li>
+          </ul>
+
+          <h4>How We Use Your Information</h4>
+          <p>
+            We may use the information we collect for the following purposes:<br/>
+            - To create and manage your account<br/>
+            - To connect you with buyers and sellers of Maize<br/>
+            - To personalize your experience within the App, using your name and user image<br/>
+            - To provide delivery services, which require your address<br/>
+            - To send you important information about the App, such as updates and service announcements<br/>
+            - To comply with legal and regulatory requirements
           </p>
 
-          <h3>Why do we use it?</h3>
+          <h4>Data Retention</h4>
           <p>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy. Various versions have evolved over the
-            years, sometimes by accident, sometimes on purpose (injected humour
-            and the like)
+            We will retain your information for as long as necessary to fulfill the purposes described in this Privacy Policy. We will also retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our agreements.
           </p>
 
+          <h4>Your Choices</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            You have choices regarding your information:<br/>
+            - You can access and update your information within the App settings.<br/>
+            - You can withdraw your consent for the collection of your location information by disabling location services in your device settings.<br/>
+            - You can contact us to request the deletion of your information.<br/>
+            - You can permanently delete your account and all data within the App and on our website at https://www.chattisemanditak.com/deleteAccount/.
           </p>
 
-          <h3>Where does it come from?</h3>
+          <h4>Security</h4>
           <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur.
+            We take reasonable steps to protect your information from unauthorized access, disclosure, alteration, or destruction. However, no internet transmission or electronic storage is ever completely secure.
           </p>
 
+          <h4>Children's Privacy</h4>
           <p>
-            But I must explain to you how all this mistaken idea of denouncing
-            pleasure and praising pain was born and I will give you a complete
-            account of the system, and expound the actual teachings of the great
-            explorer of the truth, the master-builder of human happiness. No one
-            rejects, dislikes, or avoids pleasure itself, because it is
-            pleasure, but because those who do not know how to pursue pleasure
-            rationally encounter consequences that are extremely painful. Nor
-            again is there anyone who loves or pursues or desires to obtain pain
-            of itself, because it is pain, but because occasionally
-            circumstances occur in which toil and pain can procure him some
-            great pleasure. To take a trivial example, which of us ever
-            undertakes laborious physical exercise, except to obtain some
-            advantage from it? But who has any right to find fault with a man
-            who chooses to enjoy a pleasure that has no annoying consequences,
-            or one who avoids.
+            Our App is not directed to children under the age of 13. We do not knowingly collect personal information from children under 13. If you are a parent or guardian and you believe your child has provided us with personal information, please contact us. If we become aware that we have collected personal information from a child under 13, we will take steps to delete such information from our records.
           </p>
 
-          <h3>Where can I get some?</h3>
-          <p>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </p>
+          {/* End of Privacy Policy */}
 
-          <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium voluptatum deleniti atque corrupti quos
-            dolores et quas molestias excepturi sint occaecati cupiditate non
-            provident, similique sunt in culpa qui officia deserunt mollitia
-            animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
-            est et expedita distinctio. Nam libero tempore, cum soluta nobis est
-            eligendi optio cumque nihil impedit quo minus id quod maxime placeat
-            facere possimus, omnis voluptas assumenda est, omnis dolor
-            repellendus. Temporibus autem quibusdam et aut officiis debitis aut
-            rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint
-            et molestiae non recusandae. Itaque earum rerum hic tenetur a
-            sapiente delectus, ut aut reiciendis voluptatibus maiores alias
-            consequatur aut perferendis doloribus asperiores repellat.
-          </p>
         </div>
       </div>
     </>
